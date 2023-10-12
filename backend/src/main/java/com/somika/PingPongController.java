86c22f49232bda071b348c17652d886602e5aaca
MilosZarkovic01 <miloszarkovic01@gmail.com>
2023-10-12 18:41:51 +0200

    testing deployment3

--- backend/src/main/java/com/somika/PingPongController.java
+++ backend/src/main/java/com/somika/PingPongController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
+
     private static int COUNTER = 0;
 
     record PingPong(String result) {
